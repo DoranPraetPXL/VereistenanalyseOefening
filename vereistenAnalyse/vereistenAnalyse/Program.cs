efa84cdbcb76599d1982e1df76af73262297bff8
Doran Praet <doran.praet@student.pxl.be>
2025-11-06 13:26:25 +0100

    DEMO-01

---
+++ vereistenAnalyse/vereistenAnalyse/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace vereistenAnalyse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "Administrator";
+            string password = "P@ssw0rd";
+
+            Console.WriteLine("Username: ");
+            string inputUser = Console.ReadLine();
+        }
+    }
+}
